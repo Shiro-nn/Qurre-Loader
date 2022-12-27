0e49c2b81f028d9bd2c8fdbd5b3d08d96ef0615a
Treeshold <Treeshold@treesholdapi.ml>
2022-12-27 19:05:15 +0300

    Scuffed loader (god please forgive me) Patched Assembly can be found in Dependencies

---
+++ Qurre/Loader/Features/Log.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loader.Features
+{
+    internal static class Log
+    {
+        internal static void Debug(string message, ConsoleColor color = ConsoleColor.Magenta)
+        {
+#if DEBUG
+            ServerConsole.AddLog($"[Qurre.Loader] {message}", color);
+#endif
+        }
+
+        internal static void Info(string message, ConsoleColor color = ConsoleColor.Cyan)
+        {
+            ServerConsole.AddLog($"[Qurre.Loader] {message}", color);
+        }
+
+        internal static void Warn(string message, ConsoleColor color = ConsoleColor.Yellow)
+        {
+            ServerConsole.AddLog($"[Qurre.Loader] {message}", color);
+        }
+
+        internal static void ErrorNormal(string message, ConsoleColor color = ConsoleColor.Red)
+        {
+            ServerConsole.AddLog($"[Qurre.Loader] {message}", color);
+        }
+
+        internal static void ErrorFatal(string message, ConsoleColor color = ConsoleColor.DarkRed)
+        {
+            ServerConsole.AddLog($"[Qurre.Loader] {message}", color);
+        }
+    }
+}
---
+++ Qurre/Loader/Main.cs
@@ -0,0 +1,31 @@
+﻿using Loader.Features;
+using System;
+
+namespace Loader
+{
+    /// <summary>
+    /// Entry point
+    /// </summary>
+    public class Main
+    {
+        /// <summary>
+        /// Entry point itself
+        /// </summary>
+        public void Start()
+        {
+            try
+            {
+                //todo: make a normal loader
+#if DEBUG
+                Log.Info("[DEBUG] Loading...");
+#else
+                Log.Info("Loading...");
+#endif
+            }
+            catch (Exception ex)
+            {
+                Log.ErrorFatal($"Umm, loading failed. Exception: {ex}");
+            }
+        }
+    }
+}
